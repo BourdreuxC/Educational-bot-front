372b97c2b96ad3f3276c3f8ac26411ac09c0252d
BenjaminSorriaux <sorriaux.benjamin@gmail.com>
2022-07-01 14:35:11 +0200

    Change MSAL environment variables.

--- educational-teams-bot/src/environments/environment.prod.ts
+++ educational-teams-bot/src/environments/environment.prod.ts
@@ -14,8 +14,8 @@ export const environment = {
   ],
   clientId: 'bc4ba3e1-6c39-4b50-ba44-6b6b37b7fd4d',
   tenantId: 'fefe9af7-f330-429d-8087-f5e656f7a7ce',
-  redirectUri: 'https://administration-panel.azurewebsites.net',
-  postLogoutRedirectUri: 'https://administration-panel.azurewebsites.net',
+  redirectUri: 'https://qbot-api-dev.azurewebsites.net',
+  postLogoutRedirectUri: 'https://qbot-api-dev.azurewebsites.net',
   applicationInsights: {
     instrumentationKey: '081b97a8-57ab-47fc-ac98-2b1523625a68',
   },
