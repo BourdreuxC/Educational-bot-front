2338a54032c64232c72a24f4da541d089918fdcc
Geoffrey Lioté <geoffrey.liote@diiage.org>
2022-07-04 11:43:38 +0200

    remove settings + config prod

--- educational-teams-bot/src/environments/environment.ts
+++ educational-teams-bot/src/environments/environment.ts
@@ -2,8 +2,8 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { MsalGuard, MsalInterceptor } from '@azure/msal-angular';
 
 export const environment = {
-  production: false,
-  apiEndpoint: 'http://localhost:5025/api',
+  production: true,
+  apiEndpoint: '',
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
@@ -14,7 +14,8 @@ export const environment = {
   ],
   clientId: 'bc4ba3e1-6c39-4b50-ba44-6b6b37b7fd4d',
   tenantId: 'fefe9af7-f330-429d-8087-f5e656f7a7ce',
-  redirectUri: 'http://localhost:4200',
+  redirectUri: 'https://administration-panel.azurewebsites.net',
+  postLogoutRedirectUri: 'https://administration-panel.azurewebsites.net',
   applicationInsights: {
     instrumentationKey: '081b97a8-57ab-47fc-ac98-2b1523625a68',
   },
